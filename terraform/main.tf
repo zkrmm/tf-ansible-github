a6f0bc206e083780f9ee93b9705a827c8d6faae0
administrateur <administrateur@GERJMCD.m2iformation.local>
2025-09-24 12:19:55 +0200

    Commit initial

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+# terraform/main.tf
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+variable "project" { default = "demo-tf" }
+variable "app"     { default = "nginx-unpriv" }
+variable "image"   { default = "nginxinc/nginx-unprivileged:stable" }
+
+resource "kubernetes_namespace_v1" "ns" {
+  metadata { name = var.project }
+}
+
+resource "kubernetes_deployment_v1" "deploy" {
+  metadata {
+    name      = var.app
+    namespace = var.project
+    labels    = { app = var.app }
+  }
+  spec {
+    replicas = 1
+    selector { match_labels = { app = var.app } }
+    template {
+      metadata { labels = { app = var.app } }
+      spec {
+        container {
+          name  = var.app
+          image = var.image
+          port { container_port = 8080 }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "svc" {
+  metadata {
+    name      = var.app
+    namespace = var.project
+  }
+  spec {
+    selector = { app = var.app }
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+    type = "ClusterIP"
+  }
+}
